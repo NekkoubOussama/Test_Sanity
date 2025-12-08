9d37022e3f8a27d4004b2d0998044e0ac7e9afd9
NekkoubOussama <nekkoub.oussama@gmail.com>
2025-12-08 02:01:29 +0100

    fix: bg color black

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -13,7 +13,10 @@ export default function FullLoginPage() {
           placeholder="Password"
           className="border p-2 w-full"
         />
-        <button type="submit" className="bg-blue-600 text-white px-4 py-2">
+        <button
+          type="submit"
+          className="bg-black rounded-2xl text-white px-4 py-2"
+        >
           Login
         </button>
       </form>
--- src/app/@modal/(.)login/page.tsx
+++ src/app/@modal/(.)login/page.tsx
@@ -28,7 +28,7 @@ export default function LoginModal() {
           />
           <button
             type="submit"
-            className="bg-blue-600 text-white px-4 py-2 w-full"
+            className="bg-black rounded-3xl text-white px-4 py-2 w-full"
           >
             Login
           </button>
